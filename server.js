c35431616f208bda8c5598ef35568e536bbe6ecf
GxiHub <gxi0306@hotmail.com>
2017-11-15 14:14:14 +0800

    更新測試

--- server.js
+++ server.js
@@ -258,7 +258,7 @@ app.get('/V1/API/GetMonthlySalaryForEachEmployee/',function(req,res){
   dbtest.collection('syncmonthlysalaryinformation').find({'uniID':req.headers['uniid'],'monthperiod':new RegExp(b)},{_id:0,TID:0,uniID:0}).toArray(function(err, results) {
     console.log(results);
     var jsonReturn = {};
-    if(results!=null){
+    if(results==null){
         jsonReturn = null;
     }else{
         jsonReturn ={'monthperiod':results[0].monthperiod,'monthsalary':results[0].monthsalary,'withoutsalary':results[0].withoutsalary,'foodsalary':results[0].foodsalary,'titlesalary':results[0].titlesalary,'addtimesalary':results[0].addtimesalary,'latetimesalary':results[0].latetimesalary,'extrabonus':results[0].extrabonus,'lawsalary':results[0].lawsalary,'addminute':results[0].addminute,'lateminute':results[0].lateminute,'basicsalary':results[0].basicsalary,'workdaynumber':results[0].workdaynumber,'totalmonthsalary':results[0].totalmonthsalary};
